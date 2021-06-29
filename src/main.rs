4bfdeac869d95aedfdde84cb70544c0a03737af4
marz <marcin.zajac@mobica.com>
2021-06-29 08:50:52 +0200

    args

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,84 @@
+use std::env;
+use csv::Writer;
 use regex::Regex;
+use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
-use std::error::Error;
-use csv::Writer;
 
-fn save_as_csv(r: Vec<(String, String, String)>) -> Result<(), Box<dyn Error>> {
-    let mut wtr = Writer::from_path("foo.csv")?;
+fn save_report_as_csv(r: Vec<(String, String,  String)>) -> Result<(), Box<dyn Error>> {
+    let mut wtr = Writer::from_path("report.csv")?;
+    wtr.write_record(&["Failed test", "Logged cause of error", "Optional validation errors and others and other logged information "])?;
 
     for record in r {
-        wtr.write_record(&[record.0.as_str(), record.2.as_str(), record.1.as_str()])?;
+        wtr.write_record(&[record.0.as_str(), record.1.as_str(), record.2.as_str()])?;
     }
 
     wtr.flush()?;
     Ok(())
 }
 
+
+
 fn main() {
-    let mut f = fs::File::open("test.txt").unwrap();
-    let mut buffer = String::new();
+    let args: Vec<String> = env::args().collect();
 
-    //Read all
-    f.read_to_string(&mut buffer).unwrap();
+    let f = fs::File::open(&args[1]).unwrap();
+    let mut buffer = Vec::new();
+    let f = std::io::BufReader::new(f);
+    for line in f.lines() {
+        buffer.push(line.unwrap());
+    }
 
-    //Split by empty lines
-    let cases: Vec<&str> = buffer.split("\n\r").collect();
+    let mut case: Vec<String> = Vec::new();
+    let mut cases: Vec<Vec<String>> = Vec::new();
+
+    for line in buffer {
+        if !line.is_empty() {
+            case.push(line);
+        } else {
+            cases.push(case.clone());
+            case.clear();
+        }
+    }
 
     let case_name_re = Regex::new(r"Test case '(.*)'").unwrap();
-    let validation_error_name_re = Regex::new(r"validateFunctionArguments, (.*)'").unwrap();
     let fail_couse_re = Regex::new(r"Fail \((.*?)\)$").unwrap();
 
     let mut parsed_cases: Vec<(String, String, String)> = Vec::new();
 
     for case in cases {
-        let mut case_details: (String, String, String) = (String::new(), String::new(), String::new());
-        for line in case.lines() {
+        let mut case_details: (String, String, String) =
+            (String::new(), String::new(), String::new());
+        for line in case {
             //Capture case name
-            if let Some(c) = case_name_re.captures(line) {
-                let name= c.get(1).map_or("", |m| m.as_str());
+            if let Some(c) = case_name_re.captures(line.as_str()) {
+                let name = c.get(1).map_or("", |m| m.as_str());
                 case_details.0 = String::from(name);
             }
-            //Capture optional validation errors
-            if let Some(c) = validation_error_name_re.captures(line) {
-                //First validation code
-                if case_details.1.is_empty(){
-                    let error = c.get(1).map_or("", |m| m.as_str());
-                    case_details.1 = String::from(error);
-                }
-                else {
-                    //Append another validation code
-                    let error = c.get(1).map_or("", |m| m.as_str());
-                    case_details.1.push_str("\n");    
-                    case_details.1.push_str(error);    
-                }
-            }
-            //Capture code name
-            if let Some(c) = fail_couse_re.captures(line) {
+            //Capture cause of error
+            else if let Some(c) = fail_couse_re.captures(line.as_str()) {
                 let couse = c.get(1).map_or("", |m| m.as_str());
-                case_details.2 = String::from(couse);
+                case_details.1 = String::from(couse);
+            }
+            //Capture optional validation errors
+            else {
+                if case_details.2.is_empty() {
+                    case_details.2.push_str(line.as_str());
+                } else {
+                    //Append another validation code
+                    case_details.2.push_str("\n");
+                    case_details.2.push_str(line.as_str());
+                }
             }
         }
         //Push only if error code present
-        if !case_details.2.is_empty() {
+        if !case_details.1.is_empty() {
             parsed_cases.push(case_details);
         }
     }
 
-    save_as_csv(parsed_cases).expect("Error writing to file");
+    println!("Found {} failing test", parsed_cases.len());
 
+
+    save_report_as_csv(parsed_cases).expect("Error writing to file");
 }
